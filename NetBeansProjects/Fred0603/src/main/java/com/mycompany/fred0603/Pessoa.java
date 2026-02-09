e42d869afff605dd80d84d1ab9e1621986af1db2
FredVenturin <frederico.venturin@gmail.com>
2026-02-09 20:30:35 -0300

    Add files via upload

---
+++ NetBeansProjects/Fred0603/src/main/java/com/mycompany/fred0603/Pessoa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fred0603;
+
+/**
+ *
+ * @author alunolab13
+ */
+public class Pessoa {
+    String nome;
+    int idade;
+    
+    String imprimir(){
+        return "Olá, meu nome é " + nome + " e eu tenho " + idade + " anos."; 
+    }
+    
+}
---
+++ NetBeansProjects/Fred0603/src/main/java/com/mycompany/fred0603/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fred0603;
+
+/**
+ *
+ * @author alunolab13
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Fred";
+        p1.idade = 18;
+        
+        System.out.println(p1.imprimir());
+          
+    }
+}
