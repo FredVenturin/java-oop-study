caca4b0ecd5a06d5fd33fc0283fd0a7dfdda8d8b
FredVenturin <frederico.venturin@gmail.com>
2026-02-26 20:46:27 -0300

    Add files via upload

--- fred190226/src/main/java/com/mycompany/fred190226/Conta.java
+++ fred190226/src/main/java/com/mycompany/fred190226/Conta.java
@@ -14,6 +14,7 @@ public class Conta {
 
     private double saldo;
     private double limite;
+    private double saldo_disponivel;
     private int agencia;
     private int numero;
 
@@ -22,6 +23,14 @@ public class Conta {
         this.numero = numero;
         this.saldo = saldoInicial;
         this.limite = limite;
+        atualizarSaldoDisponivel();
+    }
+
+    public Conta() {
+    }
+    
+    private void atualizarSaldoDisponivel() {
+        saldo_disponivel = saldo + limite;
     }
 
     public double getSaldo() {
@@ -32,22 +41,31 @@ public class Conta {
         return saldo + limite;
     }
 
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
     public int getAgencia() {
         return agencia;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
     }
 
     public int getNumero() {
         return numero;
     }
 
+    public void setLimite(double limite) {
+        this.limite = limite;
+        atualizarSaldoDisponivel();
+    }
+
     public double getLimite() {
         return limite;
     }
-
-    public void setLimite(double limite) {
-        this.limite = limite;
-    }
-
+    
     public void transferir(Conta destino, double valor) {
         if (debitar(valor)) {
             destino.creditar(valor);
@@ -56,15 +74,17 @@ public class Conta {
 
     public void creditar(double valor) {
         saldo += valor;
+        atualizarSaldoDisponivel();
     }
 
     public boolean debitar(double valor) {
-        if (valor > getSaldoDisponivel()) {
+        if (valor > saldo_disponivel) {
             System.out.println("ERRO: Saldo insuficiente.");
             return false;
         }
-
         saldo -= valor;
+        atualizarSaldoDisponivel();
         return true;
     }
+    
 }
